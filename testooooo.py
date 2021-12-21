2b6ce976a241c0b5c8a139ed2180841b552ec7de
i.bash <i.bash@hotmail.com>
2021-12-21 15:31:37 +0100

    add test00 hoy

---
+++ testooooo.py
@@ -0,0 +1,9 @@
+# Задача-1: Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+x = input("Enter your name: ")
+print(x)
+age = int(input("Enter your age: "))
+print(age)
+z = float(input("Choose any number: "))
+print("Nice to meet you " + x)
